5b672a15cbfc272bcc795bf2565fd19059ab8c89
jibran <jibrankhot5@gmail.com>
2024-12-04 18:21:45 +0530

    normal commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent],
-  standalone:true,
+  standalone: true,
 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
